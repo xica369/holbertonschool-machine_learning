9c1775e6a8326ff72c00a485a0da609b738a3e60
xica369 <761@holbertonschool.com>
2020-01-29 04:40:18 +0000

    T8

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""performs matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """function that performs matrix multiplication"""
+    if len(mat2) == len(mat1):
+        new_matrix = []
+        for iter in range(len(mat1)):
+            temp_list = []
+            for iter1 in range(len(mat2[0])):
+                add = 0
+                for iter2 in range(len(mat2)):
+                    for iter3 in range(len(mat2)):
+                        add = add + mat1[iter][iter2] * mat2[iter2][iter1]
+                    temp_list.append(add)
+            new_matrix.append(temp_list)
+        return new_matrix
