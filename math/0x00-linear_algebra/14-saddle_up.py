d8fef7d333f1e1d35180533521bdb1d2d3a77aa4
xica369 <761@holbertonschool.com>
2020-01-29 04:07:03 +0000

    T14: matrix multiplication with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """performs matrix multiplication"""
+    return np.dot(mat1, mat2)
