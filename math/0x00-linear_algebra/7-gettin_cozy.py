c6a38066c0983b40bfb512e32852d416bcb22757
xica369 <761@holbertonschool.com>
2020-01-29 04:30:02 +0000

    T7 add condictional

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,7 +8,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
         return mat1 + mat2
     new_mat = []
-    for iter in range(len(mat1)):
-        add = mat1[iter] + mat2[iter]
-        new_mat.append(add)
-    return new_mat
+    if len(mat1) == len(mat2) and axis == 1:
+        for iter in range(len(mat1)):
+            add = mat1[iter] + mat2[iter]
+            new_mat.append(add)
+        return new_mat
