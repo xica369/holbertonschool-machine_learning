12ad63e5c6fdcc4e575884a869f061d652423d66
xica369 <761@holbertonschool.com>
2020-01-29 03:33:01 +0000

    T10: calculates the shape of a numpy.ndarray

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """functin that calculates the shape of a numpy.ndarray"""
+    return matrix.shape
