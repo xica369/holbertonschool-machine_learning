67178aa18cdd3a43f6303289c18b5373d2e9ad20
xica369 <761@holbertonschool.com>
2020-01-29 03:52:35 +0000

    T12: operations with numpy

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""numpy: addition, subtraction, multiplication, and division"""
+
+
+def np_elementwise(mat1, mat2):
+    """numpy: addition, subtraction, multiplication, and division"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
