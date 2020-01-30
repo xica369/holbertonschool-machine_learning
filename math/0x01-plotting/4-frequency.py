f6b8b473eced4fdeae364cdbe0d06a962a2849da
xica369 <761@holbertonschool.com>
2020-01-30 23:57:34 +0000

    T4: histagram

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+plt.hist(student_grades, edgecolor='black', bins=bins)
+plt.ylim(0, 30)
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.show()
