d4e4328df53ef0de97f0a0585dc67d0d33a6d947
xica369 <761@holbertonschool.com>
2020-01-31 21:55:15 +0000

    T6: Stacking Bars with bar

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -5,11 +5,21 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
 
-bars = ('Farrah', 'Fred', 'Felicia')
-y_pos = np.arange(len(bars))
-plt.hist(fruit, stacked=True)
-plt.xticks(y_pos, bars)
-plt.title('Number of Fruit per Person')
+persons = ['Farrah', 'Fred', 'Felicia']
+fruits = ['apples', 'bananas', 'oranges', 'peaches']
+colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+w = 0.5
 plt.ylabel('Quantity of Fruit')
+ind = np.arange(3)
+plt.xticks(ind, ('Farrah', 'Fred', 'Felicia'))
 plt.ylim(0, 80, 10)
+plt.title('Number of Fruit per Person')
+
+for i in range(4):
+    bottom = np.sum(fruit[:i], axis=0)
+    label = fruits[i]
+    col = colors[i]
+    plt.bar(persons, fruit[i], label=label, width=w, color=col, bottom=bottom)
+
+plt.legend()
 plt.show()
