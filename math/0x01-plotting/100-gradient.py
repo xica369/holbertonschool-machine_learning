706087f11b9fe3ff57a399ea72f54318250c07c7
xica369 <761@holbertonschool.com>
2020-02-01 00:32:39 +0000

    T7: Gradient

---
+++ math/0x01-plotting/100-gradient.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+
+x = np.random.randn(2000) * 10
+y = np.random.randn(2000) * 10
+z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+plt.ylabel('y coordinate (m)')
+plt.xlabel('x coordinate (m)')
+plt.title('Mountain Elevation')
+
+plt.scatter(x, y, s=100, c=z)
+y = plt.colorbar()
+y.set_label('elevation (m)')
