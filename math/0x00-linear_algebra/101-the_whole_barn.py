f9d2588bd6262204b82865cb3a393aa73a6e456b
xica369 <761@holbertonschool.com>
2020-02-01 01:49:46 +0000

    T16: The Whole Barn

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""function that adds two matrices"""
+
+
+def add_matrices(mat1, mat2):
+    """function that adds two matrices"""
+    shape1 = []
+    while(mat1):
+        if isinstance(mat1, list):
+            shape.append(len(mat1))
+        if isinstance(mat1[0], list):
+            mat1 = mat1[0]
+        else:
+            mat1 = None
+
+    shape2 = []
+    while(mat2):
+        if isinstance(mat2, list):
+            shape.append(len(mat2))
+        if isinstance(mat2[0], list):
+            mat2 = mat2[0]
+        else:
+            mat2 = None
+
+    if shape1 != shape2:
+        return None
