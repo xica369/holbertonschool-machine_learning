1b6e7f651b621ac27f0fcb4dbb68dae997a1feb6
xica369 <761@holbertonschool.com>
2020-02-06 23:43:39 +0000

    T0: Initialize Poisson

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+class Poisson:
+    def __init__(self, data=None, lambtha=1.):
+        """Initialize Poisson"""
+
+        self.lambtha = float(lambtha)
+
+        if lambtha < 1:
+            raise ValueError("lambtha must be a positive value")
+
+        if data is not None and not isinstance(data, list):
+            raise TypeError("data must be a list")
+
+        if isinstance(data, list) and len(data) < 2:
+            raise ValueError("data must contain multiple values")
+
+        else:
+            lambth = float(sum(data)/len(data))
+
+        if lambtha < 1:
+            raise ValueError("lambtha must be a positive value")
