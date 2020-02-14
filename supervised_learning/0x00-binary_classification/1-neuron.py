e721a89b7d40b292da7d632f67a515975b4043f5
xica369 <761@holbertonschool.com>
2020-02-14 21:36:24 +0000

    change normal distribution

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -21,7 +21,7 @@ class Neuron:
         if nx < 1:
             raise ValueError('nx must be a positive integer')
 
-        self.__W = np.random.normal(0, 0.1, 100).tolist()
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
         print('HOLA')
