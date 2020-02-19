d60a22a4b93b286c5fbe65ec73c984c1157cf427
xica369 <761@holbertonschool.com>
2020-02-19 00:19:45 +0000

    T16: DeepNeuralNetwork

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -26,7 +26,7 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError('nx must be a positive integer')
 
-        if not isinstance(layers, list):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
@@ -34,7 +34,7 @@ class DeepNeuralNetwork:
         self.weights = {}
 
         for l in range(len(layers)):
-            if not isinstance(layers[l], int) or layers[l] < 0:
+            if not isinstance(layers[l], int) or layers[l] <= 0:
                 raise TypeError('layers must be a list of positive integers')
 
             if l == 0:
