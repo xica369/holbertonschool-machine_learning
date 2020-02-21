52aebc7db6d2b1aaf327454746ce107fe2249d14
xica369 <761@holbertonschool.com>
2020-02-21 21:17:26 +0000

    T1: One-Hot Decode

---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""One-Hot Decode"""
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """that converts a one-hot matrix into a vector of labels:
+    one_hot is a one-hot encoded numpy.ndarray with shape (classes, m)
+    classes is the maximum number of classes
+    m is the number of examples
+    Returns: a numpy.ndarray with shape (m,) containing the numeric
+    labels for each example, or None on failure"""
+
+    if not isinstance(one_hot, numpy.ndarray):
+        return None
+
+    if len(one_hot) == 0:
+        return None
