68e46148b6a489624df04520cc6717aa3ca67c60
xica369 <761@holbertonschool.com>
2020-02-21 03:10:13 +0000

    T21 again

--- supervised_learning/0x00-binary_classification/21-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/21-deep_neural_network.py
@@ -134,21 +134,18 @@ class DeepNeuralNetwork:
         m = Y.shape[1]
         cp_w = self.__weights.copy()
         la = self.__L
-
-        print(self.__cache)
-
         dz = self.__cache['A' + str(la)] - Y
-        dw = np.dot(self.__cache['A' + str(la-1)], dz.transpose())/m
+        dw = np.dot(self.__cache['A'+str(la-1)], dz.T) / m
         db = np.sum(dz, axis=1, keepdims=True)/m
 
-        self.__weights['W'+str(la)] = cp_w['W'+str(la)]-(alpha*dw.T)
-        self.__weights['b'+str(la)] = cp_w['b'+str(la)]-(alpha*db)
+        self.__weights['W'+str(la)] = cp_w['W'+str(la)] - alpha*dw.T
+        self.__weights['b'+str(la)] = cp_w['b'+str(la)] - alpha*db
 
         for la in range(self.__L - 1, 0, -1):
-            g = self.__cache['A'+str(la)]*(1-self.__cache['A'+str(la)])
-            dz = np.dot(cp_w['W'+str(la+1)].T, dz) * g
-            dw = np.dot(self.__cache['A'+str(la-1)], dz.transpose())/m
+            g = self.__cache['A'+str(la)] * (1 - self.__cache['A'+str(la)])
+            dz = np.dot(cp_w['W'+str(la+1)].T, dz)*g
+            dw = np.dot(self.__cache['A'+str(la-1)], dz.T)/m
             db = np.sum(dz, axis=1, keepdims=True)/m
 
-            self.__weights['W'+str(la)] = cp_w['W'+str(la)]-(alpha*dw.T)
-            self.__weights['b'+str(la)] = cp_w['b'+str(la)]-(alpha*db)
+            self.__weights['W'+str(la)] = cp_w['W'+str(la)] - alpha*dw.T
+            self.__weights['b'+str(la)] = cp_w['b'+str(la)] - alpha*db
