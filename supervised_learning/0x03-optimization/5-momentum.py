6b1ecf3487f4fa01efea457302839fc2bc404e84
xica369 <761@holbertonschool.com>
2020-03-03 04:29:22 +0000

    T5: Momentum

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -2,9 +2,6 @@
 
 """Momentum"""
 
-import numpy as np
-import tensorflow as tf
-
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """updates a variable using the gradient descent with
@@ -15,3 +12,8 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
     grad is a numpy.ndarray containing the gradient of var
     v is the previous first moment of var
     Returns: the updated variable and the new moment, respectively"""
+
+    v = beta1 * v + (1 - beta1) * grad
+    w = var - alpha * v
+
+    return (w, v)
