f6dc2840b30c95d3372103ccdb6aed294d61468b
xica369 <761@holbertonschool.com>
2020-03-03 07:36:50 +0000

    T12: Learning Rate Decay - tensorflow

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -16,3 +16,11 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     before alpha is decayed further
     the learning rate decay should occur in a stepwise fashion
     Returns: the learning rate decay operation"""
+
+    train = tf.train.inverse_time_decay(learning_rate=alpha,
+                                        global_step=global_step,
+                                        decay_steps=decay_step,
+                                        decay_rate=decay_rate,
+                                        staircase=True)
+
+    return train
