e3b5998238888348c1e7faa6c5c3827d3bc342d7
xica369 <761@holbertonschool.com>
2020-03-04 03:35:57 +0000

    T14

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -2,7 +2,6 @@
 
 """Batch Normalization Upgraded"""
 
-import numpy as np
 import tensorflow as tf
 
 
@@ -14,3 +13,26 @@ def create_batch_norm_layer(prev, n, activation):
     activation: activation function that should be used
     on the output of the layer
     Returns: a tensor of the activated output for the layer"""
+
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    x = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=init,
+        name='layer'
+    )
+
+    mean, variance = tf.nn.moments(x(prev), axes=0)
+
+    tensor = tf.nn.batch_normalization(
+        x,
+        mean,
+        variance,
+        offset=None,
+        scale=None,
+        variance_epsilon=1e-8,
+        name=None
+    )
+
+    return tensor
