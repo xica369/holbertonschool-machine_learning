8572b77a4928e3a76addd53d6b80227cbf9f04c3
xica369 <761@holbertonschool.com>
2020-03-08 20:25:20 +0000

    T4: update, use functions 1 and 2

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -3,16 +3,15 @@
 """F1 score"""
 
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """function that calculates the F1 score of a confusion matrix"""
 
-    TP = np.diag(confusion)
-    FP = confusion.sum(axis=0) - TP
-    FN = confusion.sum(axis=1) - TP
-    recall = TP / (TP + FN)
-    precision = TP / (TP + FP)
-    F1_score = 2 * (precision * recall) / (precision + recall)
+    _sensitivity = sensitivity(confusion)
+    _precision = precision(confusion)
+    F1_score = 2 * (_precision * _sensitivity) / (_precision + _sensitivity)
 
     return F1_score
