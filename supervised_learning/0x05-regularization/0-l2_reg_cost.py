cf0807aeb504b95b749cfac3e30f321084f06672
xica369 <761@holbertonschool.com>
2020-03-11 19:54:28 +0000

    T0 add keepdims=true

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -15,9 +15,11 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     m is the number of data points used
     Returns: the cost of the network accounting for L2 regularization"""
 
-    for layer in range(L):
-        sum_w = (np.linalg.norm(weights['W' + str(layer + 1)])) ** 2
+    sum_w = 0
 
-    L2_cost = cost + lambtha / (2 * m) * sum_w
+    for layer in range(L):
+        sum_w += np.linalg.norm(weights['W' + str(layer + 1)], keepdims=True)
+
+    L2_cost = cost + lambtha / (2 * m) * np.sum(sum_w)
 
     return L2_cost
