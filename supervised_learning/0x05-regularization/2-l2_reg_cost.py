32039d05217fc1c82441dda3a8e7860afd7a841b
xica369 <761@holbertonschool.com>
2020-03-11 19:58:47 +0000

    T2: L2 Regularization Cost

---
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""L2 Regularization Cost"""
+
+import numpy as np
+
+
+def l2_reg_cost(cost):
+    """calculates the cost of a neural network with L2 regularization:
+
+    cost is a tensor with the cost of the network without L2 regularization
+    Returns: a tensor with the cost of the network"""
+
+    return cost
