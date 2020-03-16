c31be9a6253e2d9be21737a4728b0370f5fff2cc
xica369 <761@holbertonschool.com>
2020-03-16 21:15:23 +0000

    T1: Input

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""Input"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """builds a neural network with the Keras library:
+
+    nx: number of input features to the network
+    layers: list containing the number of nodes in each layer of the network
+    activations: list with the activation functions used for each layer
+    lambtha: the L2 regularization parameter
+    keep_prob: the probability that a node will be kept for dropout
+    Returns: the keras model"""
+
+    inputs = K.Input(shape=(nx,))
+    output = K.layers.Dense(
+        layers[0],
+        activation=activations[0],
+        kernel_regularizer=K.regularizers.l2(lambtha),
+        name="dense")(inputs)
+
+    for layer in range(1, len(layers)):
+        dropout = K.layers.Dropout(keep_prob)(output)
+        outputs = K.layers.Dense(
+            layers[layer],
+            activation=activations[layer],
+            kernel_regularizer=K.regularizers.l2(lambtha),
+            name="dense_" + str(layer))(dropout)
+        print(layers[layer])
+
+    model = K.models.Model(inputs=inputs, outputs=outputs)
+
+    return model
