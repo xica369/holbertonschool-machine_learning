be319b35aaa031b3f8c48f84d6a83c1bb494812c
xica369 <761@holbertonschool.com>
2020-03-16 21:54:11 +0000

    T2 other way

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -15,9 +15,10 @@ def optimize_model(network, alpha, beta1, beta2):
     beta2 is the second Adam optimization parameter
     Returns: None"""
 
-    network.compile(optimizer=K.optimizers.Adam(learning_rate=alpha,
-                                                beta_1=beta1, beta_2=beta2),
-                    loss=K.losses.CategoricalCrossentropy(from_logits=True),
+    adam = Adam(alpha, beta1, beta2)
+
+    network.compile(loss="categorical_crossentropy",
+                    optimizer=adam,
                     metrics=['accuracy'])
 
     return None
