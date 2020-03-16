ba8251fce8fb84e7f60afe9a824fc2591388906c
xica369 <761@holbertonschool.com>
2020-03-16 17:07:17 +0000

    T2: add complile

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -15,7 +15,8 @@ def optimize_model(network, alpha, beta1, beta2):
     beta2 is the second Adam optimization parameter
     Returns: None"""
 
-    network.optimizers.Adam(
-        learning_rate=alpha,
-        beta_1=beta1,
-        beta_2=beta2)
+    network.compile(optimizer=K.optimizers.Adam(learning_rate=alpha,
+                                                beta_1=beta1,
+                                                beta_2=beta2))
+
+    return None
