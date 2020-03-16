6926fc0582c330039a8d878bec4ddd73545bc956
xica369 <761@holbertonschool.com>
2020-03-16 22:06:43 +0000

    T4: train

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Train with mini-batch gradient decet"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True,
+                shuffle=False):
+    """function that trains a model using mini-batch gradient descent"""
+
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle)
+
+    return history
