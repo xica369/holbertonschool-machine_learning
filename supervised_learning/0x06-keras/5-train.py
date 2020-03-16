ad5f48eb9d90fa500634ba2f76b04802aedca53b
xica369 <761@holbertonschool.com>
2020-03-16 22:22:43 +0000

    train with validation_data

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -9,13 +9,12 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """function that trains a model using mini-batch gradient descent"""
 
-    history = None
-    if validation_data:
-        history = network.fit(x=data,
-                              y=labels,
-                              batch_size=batch_size,
-                              epochs=epochs,
-                              verbose=verbose,
-                              shuffle=shuffle)
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          validation_data=validation_data)
 
     return history
