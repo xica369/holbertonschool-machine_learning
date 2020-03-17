a132808b32bb72a028c1aa009820b2a62fdb77ef
xica369 <761@holbertonschool.com>
2020-03-17 03:36:16 +0000

    T10 save and load weights

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""Save and Load Weights"""
+
+
+def save_weights(network, filename, save_format='h5'):
+    """saves a model’s weights:
+    network is the model whose weights should be saved
+    filename is the path of the file that the weights should be saved to
+    save_format is the format in which the weights should be saved
+    Returns: None"""
+
+    network.save_weights(filename, save_format=save_format)
+
+    return None
+
+
+def load_weights(network, filename):
+    """loads a model’s weights:
+    network is the model to which the weights should be loaded
+    filename is the path of the file that the weights should be loaded from
+    Returns: None"""
+
+    network.load_weights(filename)
+
+    return None
