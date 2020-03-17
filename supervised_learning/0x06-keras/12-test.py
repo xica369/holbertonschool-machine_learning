004450aece99917261828a02b7663574eda61c45
xica369 <761@holbertonschool.com>
2020-03-17 04:37:05 +0000

    T12: test

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+"""Test function"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """tests a neural network:
+
+    network is the network model to test
+    data is the input data to test the model with
+    labels are the correct one-hot labels of data
+    verbose is a boolean that determines if output should be printed
+    during the testing process
+    Returns: the loss and accuracy of the model with the testing data"""
+
+    test_model = network.evaluate(data, labels, verbose=verbose)
+
+    loss = test_model[0]
+    accuracy = test_model[1]
+
+    return (loss, accuracy)
