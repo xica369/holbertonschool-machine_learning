6b8aeeadc7e44ad0a9f761c36d806b175035bb52
xica369 <761@holbertonschool.com>
2020-03-19 04:58:16 +0000

    T3: Strided Convolution

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -3,12 +3,12 @@
 """function that performs a convolution on grayscale images:
 
 images: numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-m: is the number of images
-h: is the height in pixels of the images
-w: is the width in pixels of the images
+  m: is the number of images
+  h: is the height in pixels of the images
+  w: is the width in pixels of the images
 kernel: numpy.ndarray with shape (kh, kw) with the kernel for the convolution
-kh: is the height of the kernel
-kw: is the width of the kernel
+  kh: is the height of the kernel
+  kw: is the width of the kernel
 padding: is either a tuple of (ph, pw), ‘same’, or ‘valid’
 if ‘same’, performs a same convolution
 if ‘valid’, performs a valid convolution
@@ -16,8 +16,8 @@ if a tuple:
   ph is the padding for the height of the image
   pw is the padding for the width of the image
 stride: is a tuple of (sh, sw)
-sh: is the stride for the height of the image
-sw: is the stride for the width of the image
+  sh: is the stride for the height of the image
+  sw: is the stride for the width of the image
 
 Returns: a numpy.ndarray containing the convolved images"""
 
@@ -26,3 +26,42 @@ import numpy as np
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """Strided Convolution"""
+
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+
+    sh = stride[0]
+    sw = stride[1]
+
+    if isinstance(padding, tuple):
+        p_h = padding[0]
+        p_w = padding[1]
+
+    if padding == "same":
+        p_h = int((kh - 1) / 2)
+        p_w = int((kw - 1) / 2)
+
+    if padding == "valid":
+        p_h = 0
+        p_w = 0
+
+    output_h = int((h + (2 * p_h) - kh) / sh) + 1
+    output_w = int((w + (2 * p_w) - kw) / sw) + 1
+
+    output = np.zeros((m, output_h, output_w))
+
+    image = np.arange(m)
+    imag_p = np.pad(images, ((0, 0), (p_h, p_h), (p_w, p_w)), mode="symmetric")
+
+    for height in range(output_h):
+        for width in range(output_w):
+            output[image, height, width] = (np.sum(imag_p[image,
+                                            height*sh:(height*sh)+kh,
+                                            width*sh:(width*sh)+kw] *
+                                            kernel, axis=(1, 2)))
+
+    return output
