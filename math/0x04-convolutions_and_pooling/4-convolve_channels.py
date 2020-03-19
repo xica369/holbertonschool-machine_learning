7eedb3935891bd489b48be37b1118fbd5621cfe2
xica369 <761@holbertonschool.com>
2020-03-19 05:00:56 +0000

    T4

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -7,7 +7,7 @@ m: is the number of images
 h: is the height in pixels of the images
 w: is the width in pixels of the images
 c: is the number of channels in the image
-kernel: numpy.ndarray with shape (kh, kw, c) with the kernel for the convolution
+kernel: numpy.ndarray with shape (kh, kw, c) with kernel for the convolution
 kh: is the height of the kernel
 kw: is the width of the kernel
 padding: is either a tuple of (ph, pw), ‘same’, or ‘valid’
@@ -27,3 +27,42 @@ import numpy as np
 
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """Convolution with Channels"""
+
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+
+    sh = stride[0]
+    sw = stride[1]
+
+    if isinstance(padding, tuple):
+        p_h = padding[0]
+        p_w = padding[1]
+
+    if padding == "same":
+        p_h = int((kh - 1) / 2)
+        p_w = int((kw - 1) / 2)
+
+    if padding == "valid":
+        p_h = 0
+        p_w = 0
+
+    output_h = int((h + (2 * p_h) - kh) / sh) + 1
+    output_w = int((w + (2 * p_w) - kw) / sw) + 1
+
+    output = np.zeros((m, output_h, output_w))
+
+    image = np.arange(m)
+    imag_p = np.pad(images, ((0, 0), (p_h, p_h), (p_w, p_w)), mode="symmetric")
+
+    for height in range(output_h):
+        for width in range(output_w):
+            output[image, height, width] = (np.sum(imag_p[image,
+                                            height*sh:(height*sh)+kh,
+                                            width*sh:(width*sh)+kw] *
+                                            kernel, axis=(1, 2)))
+
+    return output
