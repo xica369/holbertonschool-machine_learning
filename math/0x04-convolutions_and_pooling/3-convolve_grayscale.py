1fc06d1e16330caf902d215584dbb92f63a31c06
xica369 <761@holbertonschool.com>
2020-03-21 04:40:30 +0000

    T3 again

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -45,8 +45,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         oh = int(h / sh)
         ow = int(w / sw)
 
-        p_h = 1 + int((oh - 1) * sh + kh - h) / 2
-        p_w = 1 + int((ow - 1) * sw + kw - w) / 2
+        p_h = 1 + int(((oh - 1) * sh + kh - h) / 2)
+        p_w = 1 + int(((ow - 1) * sw + kw - w) / 2)
 
     if padding == "valid":
         p_h = 0
