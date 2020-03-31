e47dc7ab1ece6af5918b601cd3c31d601979ae11
xica369 <761@holbertonschool.com>
2020-03-31 17:51:54 +0000

    T1: add layer

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -29,57 +29,61 @@ def inception_network():
                                    strides=2,
                                    padding="same")(layer1)
 
-    layer3 = K.layers.Conv2D(filters=192,
-                             kernel_size=3,
+    layer3 = K.layers.Conv2D(filters=64,
+                             kernel_size=1,
                              padding="same",
-                             strides=1,
                              kernel_initializer=he_normal,
                              activation="relu")(layer2)
 
-    layer4 = K.layers.MaxPooling2D(pool_size=3,
+    layer4 = K.layers.Conv2D(filters=192,
+                             kernel_size=3,
+                             padding="same",
+                             kernel_initializer=he_normal,
+                             activation="relu")(layer3)
+
+    layer5 = K.layers.MaxPooling2D(pool_size=3,
                                    strides=2,
-                                   padding="same")(layer3)
+                                   padding="same")(layer4)
 
     filters = [64, 96, 128, 16, 32, 32]
-    layer5 = inception_block(layer4, filters)
-
-    filters = [128, 128, 192, 32, 96, 64]
     layer6 = inception_block(layer5, filters)
 
-    layer7 = K.layers.MaxPooling2D(pool_size=3,
+    filters = [128, 128, 192, 32, 96, 64]
+    layer7 = inception_block(layer6, filters)
+
+    layer8 = K.layers.MaxPooling2D(pool_size=3,
                                    strides=2,
-                                   padding="same")(layer6)
+                                   padding="same")(layer7)
 
     filters = [192, 96, 208, 16, 48, 64]
-    layer8 = inception_block(layer7, filters)
-
-    filters = [160, 112, 224, 24, 64, 64]
     layer9 = inception_block(layer8, filters)
 
-    filters = [128, 128, 256, 24, 64, 64]
+    filters = [160, 112, 224, 24, 64, 64]
     layer10 = inception_block(layer9, filters)
 
-    filters = [112, 144, 288, 32, 64, 64]
+    filters = [128, 128, 256, 24, 64, 64]
     layer11 = inception_block(layer10, filters)
 
-    filters = [256, 160, 320, 32, 128, 128]
+    filters = [112, 144, 288, 32, 64, 64]
     layer12 = inception_block(layer11, filters)
 
-    layer13 = K.layers.MaxPooling2D(pool_size=3,
+    filters = [256, 160, 320, 32, 128, 128]
+    layer13 = inception_block(layer12, filters)
+
+    layer14 = K.layers.MaxPooling2D(pool_size=3,
                                     strides=2,
-                                    padding="same")(layer12)
+                                    padding="same")(layer13)
 
     filters = [256, 160, 320, 32, 128, 128]
-    layer14 = inception_block(layer13, filters)
-
-    filters = [384, 192, 384, 48, 128, 128]
     layer15 = inception_block(layer14, filters)
 
-    layer16 = K.layers.AveragePooling2D(pool_size=7,
-                                        padding="same",
-                                        strides=1)(layer15)
+    filters = [384, 192, 384, 48, 128, 128]
+    layer16 = inception_block(layer15, filters)
 
-    dropout = K.layers.Dropout(rate=0.4)(layer16)
+    layer17 = K.layers.AveragePooling2D(pool_size=7,
+                                        padding="same")(layer16)
+
+    dropout = K.layers.Dropout(rate=0.4)(layer17)
 
     softmax = K.layers.Dense(units=1000,
                              activation="softmax")(dropout)
