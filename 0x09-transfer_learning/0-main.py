18bdc866484005ec6174553ab84e81dbfee19876
xica369 <761@holbertonschool.com>
2020-04-15 14:32:27 +0000

    remove comments

--- 0x09-transfer_learning/0-main.py
+++ 0x09-transfer_learning/0-main.py
@@ -8,11 +8,7 @@ K.learning_phase = K.backend.learning_phase
 
 _, (X, Y) = K.datasets.cifar10.load_data()
 X_p, Y_p = preprocess_data(X, Y)
-<<<<<<< HEAD
+
 model = K.models.load_model('cifar10.h5')
 evaluate = model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
 print(evaluate)
-=======
-#model = K.models.load_model('cifar10.h5')
-#model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
->>>>>>> 8e3bed36e29daaba7ebd7c4f05ebec1bc54f8097
