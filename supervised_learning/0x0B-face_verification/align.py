df57297634528af75638960a8f4c989b917eb851
xica369 <761@holbertonschool.com>
2020-04-25 04:20:04 +0000

    T3: Detect Faces

--- supervised_learning/0x0B-face_verification/align.py
+++ supervised_learning/0x0B-face_verification/align.py
@@ -16,3 +16,32 @@ class FaceAlign:
 
         self.detector = dlib.get_frontal_face_detector()
         self.shape_predictor = dlib.shape_predictor(shape_predictor_path)
+
+    def detect(self, image):
+        """method that detects a face in an image"""
+
+        try:
+            detected_faces = self.detector(image, 1)
+
+            if len(detected_faces) == 0:
+                x, y, w, h = 0, 0, image.shape[1], image.shape[0]
+                box_face = dlib.rectangle(x, y, w, h)
+
+            else:
+                max_area = 0
+                for fc in detected_faces:
+                    _x = fc.left()
+                    _y = fc.top()
+                    _w = fc.width()
+                    _h = fc.height()
+
+                    if _w * _h > max_area:
+                        x, y, w, h = _x, _y, _w, _h
+                        max_area = w * h
+
+                box_face = dlib.rectangle(x, y, (w + x), (h + y))
+
+            return box_face
+
+        except Exception:
+            return None
