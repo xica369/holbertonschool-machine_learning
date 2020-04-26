9420254a593a269cbdf5da17db1d396f62c5516e
xica369 <761@holbertonschool.com>
2020-04-26 03:29:49 +0000

    T9: Calculate Triplet Loss

--- supervised_learning/0x0B-face_verification/triplet_loss.py
+++ supervised_learning/0x0B-face_verification/triplet_loss.py
@@ -3,6 +3,7 @@
 """Create a custom layer class TripletLoss"""
 
 import tensorflow
+import tensorflow as tf
 
 
 class TripletLoss(tensorflow.keras.layers.Layer):
@@ -16,3 +17,21 @@ class TripletLoss(tensorflow.keras.layers.Layer):
 
         self.alpha = alpha
         super(TripletLoss, self).__init__(**kwargs)
+
+    def triplet_loss(self, inputs):
+        """method that calculate Triplet Loss:
+        inputs is a list containing the anchor, positive and
+        negative output tensors from the last layer of the model, respectively
+        Returns: a tensor containing the triplet loss values"""
+
+        positive_output = inputs[1]
+        negative_output = inputs[2]
+        anchor_output = inputs[0]
+
+        d_posit = tf.reduce_sum(tf.square(anchor_output - positive_output), 1)
+        d_negat = tf.reduce_sum(tf.square(anchor_output - negative_output), 1)
+
+        margin = self.alpha
+        loss = tf.maximum(margin + d_posit - d_negat, 0)
+
+        return loss
