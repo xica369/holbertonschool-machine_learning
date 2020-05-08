6bb7f659b0171963298b9da93a390648181c166f
xica369 <761@holbertonschool.com>
2020-05-08 00:28:07 +0000

    T4: change order to functions

--- math/0x05-advanced_linear_algebra/4-inverse.py
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -5,7 +5,6 @@
 
 def inverse(matrix):
     """matrix is a list of lists whose inverse should be calculated
-
     Returns: the inverse of matrix, or None if matrix is singular"""
 
     if not isinstance(matrix, list):
@@ -28,24 +27,40 @@ def inverse(matrix):
     if len(matrix[0]) == 1:
         return [[1 / matrix[0][0]]]
 
+    adjugate_matrix = adjugate(matrix)
     val_determinant = determinant(matrix)
     if val_determinant == 0:
         return None
 
-    cofactor_matrix = cofactor(matrix)
     inverse_matrix = []
+    for row in adjugate_matrix:
+        temp = []
+        for item in row:
+            temp.append(item / val_determinant)
+        inverse_matrix.append(temp)
+    return inverse_matrix
 
+
+def adjugate(matrix):
+    """matrix is a list of lists whose adjugate matrix should be calculated
+
+    Returns: the adjugate matrix of matrix"""
+
+    if len(matrix[0]) == 1:
+        return [[1]]
+
+    cofactor_matrix = cofactor(matrix)
+    adjugate_matrix = []
     for iter in range(len(cofactor_matrix)):
         temp_list = []
         for row in cofactor_matrix:
-            temp_list.append(row[iter] / val_determinant)
-        inverse_matrix.append(temp_list)
-    return inverse_matrix
+            temp_list.append(row[iter])
+        adjugate_matrix.append(temp_list)
+    return adjugate_matrix
 
 
 def cofactor(matrix):
     """matrix is a list of lists whose cofactor matrix should be calculated
-
     Returns: the cofactor matrix of matrix"""
 
     if len(matrix[0]) == 1:
@@ -79,7 +94,6 @@ def calc_sub_matrix(matrix, row, column):
 def determinant(matrix):
     """matrix is a list of lists whose determinant should be calculated
     The list [[]] represents a 0x0 matrix
-
     Returns: the determinant of matrix"""
 
     if matrix == [[]]:
@@ -90,32 +104,33 @@ def determinant(matrix):
 
     # bottom triangle at zero with Gauss method
     signo = 1
-    for row in range(len(matrix)):
+    cp_matrix = matrix.copy()
+    for row in range(len(cp_matrix)):
         # row change when the diagonal element is 0
-        if matrix[row][row] == 0:
-            copy_row = matrix[row]
+        if cp_matrix[row][row] == 0:
+            copy_row = cp_matrix[row]
             for row2 in range(row + 1, len(matrix)):
-                if matrix[row2][row] != 0:
-                    matrix[row] = matrix[row2]
-                    matrix[row2] = copy_row
+                if cp_matrix[row2][row] != 0:
+                    cp_matrix[row] = cp_matrix[row2]
+                    cp_matrix[row2] = copy_row
                     signo *= -1
                     break
 
-        if matrix[row][row] == 0:
+        if cp_matrix[row][row] == 0:
             return 0
 
         # put zero the column elements
-        for row2 in range(row + 1, len(matrix)):
-            temp = matrix[row2][row] / matrix[row][row]
-            for colum in range(row, len(matrix)):
-                pos1 = matrix[row2][colum]
-                pos2 = temp * matrix[row][colum]
-                matrix[row2][colum] = pos1 - pos2
+        for row2 in range(row + 1, len(cp_matrix)):
+            temp = cp_matrix[row2][row] / cp_matrix[row][row]
+            for colum in range(row, len(cp_matrix)):
+                pos1 = cp_matrix[row2][colum]
+                pos2 = temp * cp_matrix[row][colum]
+                cp_matrix[row2][colum] = pos1 - pos2
 
     # determinant calculation
     determinant = 1
-    for row in range(len(matrix)):
-        determinant *= matrix[row][row]
+    for row in range(len(cp_matrix)):
+        determinant *= cp_matrix[row][row]
 
     determinant *= signo
 
