fdddfc6d6c98d53024fb6b1886c58349f820762c
xica369 <761@holbertonschool.com>
2020-05-12 01:44:23 +0000

    change menssage

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -50,17 +50,17 @@ class MultiNormal:
         """
 
         if not isinstance(x, np.ndarray):
-            raise TypeError("x must by a numpy.ndarray")
+            raise TypeError("x must be a numpy.ndarray")
 
         if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != self.d:
-            raise ValueError("x mush have the shape ({}, 1)".format(self.d))
+            raise ValueError("x must have the shape ({}, 1)".format(self.d))
 
         mean = self.mean
         cov = self.cov
 
-        part1 = 1/(((2*np.pi) ** (len(mean)/2)) * (np.linalg.det(cov)**(1/2)))
-        part2 = (-1/2) * ((x-mean).T.dot(np.linalg.inv(cov))).dot((x-mean))
+        base = 1/(((2*np.pi) ** (len(mean)/2)) * (np.linalg.det(cov)**(1/2)))
+        expo = (-1/2) * ((x-mean).T.dot(np.linalg.inv(cov))).dot((x-mean))
 
-        pdf = float(part1 * np.exp(part2))
+        pdf = float(base * np.exp(expo))
 
         return pdf
