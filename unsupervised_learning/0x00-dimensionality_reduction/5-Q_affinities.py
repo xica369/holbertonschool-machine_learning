790c9226bd44c371d14fa609bf9278a14670daae
xica369 <761@holbertonschool.com>
2020-05-16 02:46:58 +0000

    T5: Q affinities

--- unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -18,3 +18,19 @@ def Q_affinities(Y):
     Q: numpy.ndarray of shape (n, n) containing the Q affinities
     num: numpy.ndarray of shape (n, n) with the numerator of the Q affinities
     """
+
+    n = Y.shape[0]
+    Q = np.zeros((n, n))
+
+    sum_Y = np.sum(np.square(Y), axis=1, keepdims=True)
+
+    # similar to (a-b)^2 = a^2 + b^2 - 2*a*b
+    num = sum_Y + sum_Y.T - 2 * np.dot(Y, Y.T)
+    num = (1 + num) ** -1
+
+    # put diagonal in zeros
+    num[range(n), range(n)] = 0
+
+    Q = num / np.sum(num)
+
+    return(Q, num)
