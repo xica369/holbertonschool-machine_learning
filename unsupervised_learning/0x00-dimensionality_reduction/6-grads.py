bdafa24b12a70ce160325790397d2e57baaaf564
xica369 <761@holbertonschool.com>
2020-05-16 03:45:11 +0000

    T6: gradients

--- unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -3,6 +3,7 @@
 """Gradients"""
 
 import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
 
 
 def grads(Y, P):
@@ -17,3 +18,15 @@ def grads(Y, P):
     dY is a numpy.ndarray of shape (n, n) containing the gradients of Y
     Q is a numpy.ndarray of shape (n, n) containing the Q affinities of Y
     """
+
+    n, m = Y.shape
+
+    Q, num = Q_affinities(Y)
+    dY = np.zeros((n, m))
+    PQ = P - Q
+
+    for iter in range(n):
+        dY[iter, :] = np.sum(np.tile(PQ[:, iter] * num[:, iter],
+                                     (m, 1)).T * (Y[iter, :]-Y), axis=0)
+
+    return dY, Q
