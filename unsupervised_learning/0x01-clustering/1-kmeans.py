dae91981348365acd93be49e3b9fe21bd4617627
xica369 <761@holbertonschool.com>
2020-05-21 14:41:43 +0000

    k-means

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -21,3 +21,67 @@ import numpy as np
 
 def kmeans(X, k, iterations=1000):
     """K-means"""
+
+    C = initialize(X, k)
+
+    if not isinstance(iterations, int) or iterations < 1:
+        return None
+
+    try:
+        d = X.shape[1]
+        low = np.amin(X, axis=0)
+        high = np.amax(X, axis=0)
+        c_temp = C.copy()
+
+        # calculate centroides
+        for i in range(iterations):
+
+            # calculate Euclidean distance between data and centroids
+            distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
+
+            # take the shortest distance
+            clss = np.argmin(distances, axis=0)
+
+            # update position of centroids
+            for k in range(C.shape[0]):
+
+                # If a cluster does not have data points, reset it
+                if (len(X[clss == k]) == 0):
+                    C[k, :] = np.random.uniform(low, high, size=(1, d))
+                else:
+                    C[k, :] = (X[clss == k].mean(axis=0))
+
+            # check if the centroids have stabilized
+            if (c_temp == C).all():
+                return C, clss
+
+            c_temp = C.copy()
+
+    except Exception:
+        return None
+
+    return C, clss
+
+
+def initialize(X, k):
+    """Initialize K-means"""
+
+    if not isinstance(k, int) or k < 1:
+        return None
+
+    try:
+        d = X.shape[1]
+
+        # minimum values of X along each dimension in d
+        low = np.amin(X, 0)
+
+        # maximum values of X along each dimension in d
+        high = np.amax(X, 0)
+
+        # initialize with a multivariate uniform distribution
+        initialization = np.random.uniform(low=low, high=high, size=(k, d))
+
+    except Exception:
+        return None
+
+    return initialization
