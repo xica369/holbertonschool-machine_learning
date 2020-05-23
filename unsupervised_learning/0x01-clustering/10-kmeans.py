1984a0e6001842f260f44a0b50f3a957cf036a4e
xica369 <761@holbertonschool.com>
2020-05-23 01:29:53 +0000

    T10: Hello, sklearn!

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -18,3 +18,10 @@ import sklearn.cluster
 
 def kmeans(X, k):
     """Hello, sklearn"""
+
+    kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+
+    return C, clss
