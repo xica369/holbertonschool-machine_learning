c3910694a0a16f6d01501a8bf177b4a8f0735ad9
xica369 <761@holbertonschool.com>
2020-05-23 01:45:24 +0000

    T11: GMM

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -21,3 +21,13 @@ import sklearn.mixture
 
 def gmm(X, k):
     """calculates a GMM from a dataset"""
+
+    g = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
+
+    pi = g.weights_
+    m = g.means_
+    S = g.covariances_
+    clss = g.predict(X)
+    bic = g.bic(X)
+
+    return pi, m, S, clss, bic
