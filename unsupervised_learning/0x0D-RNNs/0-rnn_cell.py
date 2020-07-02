d0e41b9fa853d78d3902369a5cd9e5968615435f
xica369 <761@holbertonschool.com>
2020-07-02 20:15:03 +0000

    RNN Cell

--- unsupervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ unsupervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -14,8 +14,8 @@ class RNNCell:
         h is the dimensionality of the hidden state
         o is the dimensionality of the outputs
 
-        Public instance attributes Wh, Wy, bh, by that represent the weights and
-        biases of the cell
+        Public instance attributes Wh, Wy, bh, by that represent the weights
+        and biases of the cell:
         Wh and bh are for the concatenated hidden state and input data
         Wy and by are for the output
         """
@@ -23,7 +23,6 @@ class RNNCell:
         # weights
         self.Wh = np.random.normal(size=(i+h, h))
         self.Wy = np.random.normal(size=(h, o))
-        self.Whh = np.random.normal(size=(h, h))
 
         # biases
         self.bh = np.zeros((1, h))
@@ -44,8 +43,18 @@ class RNNCell:
         y is the output of the cell
         """
 
-        m, i = x_t.shape[0]
-        h_next = np.zeros((m, self.bh.shape[1]))
-        y = np.zeros((m, self.by.shape[1]))
+        Wx = np.concatenate((h_prev, x_t), axis=1)
+
+        # h_next = tanh(Wh[h_prev, x_t] + bh)
+        h_next = np.tanh(np.dot(Wx, self.Wh) + self.bh)
+
+        # y = softmax(Wy * h_next + by)
+        y = self.softmax(np.dot(h_next, self.Wy) + self.by)
 
         return h_next, y
+
+    def softmax(self, X):
+        """
+        softmax function
+        """
+        return np.exp(X) / np.sum(np.exp(X), axis=1, keepdims=True)
