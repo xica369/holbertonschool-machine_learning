68200d9cc7e82a6b0c4e970159888f084e24211f
xica369 <761@holbertonschool.com>
2020-07-12 21:49:31 +0000

    Train Discriminator

--- unsupervised_learning/0x05-GANs/2-train_discriminator.py
+++ unsupervised_learning/0x05-GANs/2-train_discriminator.py
@@ -21,3 +21,26 @@ def train_discriminator(Z, X):
     """
     Function that creates the loss tensor and training op for the discriminator
     """
+
+    # generator model
+    generator_sample = generator(Z)
+
+    # discriminator model
+    discriminator_real = discriminator(X)
+    discriminator_fake = discriminotor(generator_sample)
+
+    # loss function
+    loss = -tf.reduce_mean(tf.log(discriminator_real) +
+                           tf.log(1 - discriminator_fake))
+
+    # select parameters
+    disc_vars = []
+
+    for variable in tf.trainable_variables():
+        if variable.name.startwith("discr"):
+            disc_vars.append(variable)
+
+    # optimizer
+    train_op = tf.train.AdamOptimizer().minimize(loss, var_list=disc_vars)
+
+    return loss, train_op
