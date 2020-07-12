8658a493033706fefacf1c48ea5215e4ace7619d
xica369 <761@holbertonschool.com>
2020-07-12 22:03:32 +0000

    Train Generator

--- unsupervised_learning/0x05-GANs/3-train_generator.py
+++ unsupervised_learning/0x05-GANs/3-train_generator.py
@@ -11,7 +11,6 @@ loss is the generator loss
 train_op is the training operation for the generator
 """
 
-import numpy as np
 import tensorflow as tf
 generator = __import__('0-generator').generator
 discriminator = __import__('1-discriminator').discriminator
@@ -21,3 +20,24 @@ def train_generator(Z):
     """
     Function that creates the loss tensor and training op for the generator
     """
+
+    # generator model
+    generator_sample = generator(Z)
+
+    # discriminator model
+    discriminator_fake = discriminator(generator_sample)
+
+    # loss function
+    loss = -tf.reduce_mean(tf.log(discriminator_fake))
+
+    # select parameters
+    gen_vars = []
+
+    for variable in tf.trainable_variables():
+        if variable.name.startwith("gener"):
+            gen_vars.append(variable)
+
+    # optimizer
+    train_op = tf.train.AdamOptimizer().minimize(loss, var_list=gen_vars)
+
+    return loss, train_op
