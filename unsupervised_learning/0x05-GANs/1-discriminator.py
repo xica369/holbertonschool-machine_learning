65619b78a48584828e926b742b834b0226a1fbb7
xica369 <761@holbertonschool.com>
2020-07-12 21:14:44 +0000

    discriminator

--- unsupervised_learning/0x05-GANs/1-discriminator.py
+++ unsupervised_learning/0x05-GANs/1-discriminator.py
@@ -16,3 +16,15 @@ def discriminator(X):
     """
     Function that creates a discriminator network for MNIST digits
     """
+
+    with tf.variable_scope("discriminator", reuse=tf.AUTO_REUSE):
+
+        layer_1 = tf.layers.Dense(128,
+                                  activation="relu",
+                                  name="layer_1")(X)
+
+        layer_2 = tf.layers.Dense(1,
+                                  activation="sigmoid",
+                                  name="layer_2")(layer_1)
+
+    return layer_2
