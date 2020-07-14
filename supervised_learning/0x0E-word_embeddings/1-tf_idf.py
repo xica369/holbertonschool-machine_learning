70120dffa3a85170e2d370bf855a6eea95308a62
xica369 <761@holbertonschool.com>
2020-07-14 20:47:50 +0000

    Basic structure

---
+++ supervised_learning/0x0E-word_embeddings/1-tf_idf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+TF-IDF
+"""
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Function that creates a TF-IDF embedding:
+
+    - sentences is a list of sentences to analyze
+    - vocab is a list of the vocabulary words to use for the analysis
+        If None, all words within sentences should be used
+
+    Returns: embeddings, features
+
+    - embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+        s is the number of sentences in sentences
+        f is the number of features analyzed
+    - features is a list of the features used for embeddings
+    """
