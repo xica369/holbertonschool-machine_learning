9da1123333b3f030d9c8427d9c82d0ca85151c7e
xica369 <761@holbertonschool.com>
2020-07-15 03:33:23 +0000

    Bag Of Words

--- supervised_learning/0x0E-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0E-word_embeddings/0-bag_of_words.py
@@ -4,6 +4,8 @@
 Bag Of Words
 """
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 def bag_of_words(sentences, vocab=None):
     """
@@ -19,3 +21,10 @@ def bag_of_words(sentences, vocab=None):
       f is the number of features analyzed
     - features is a list of the features used for embeddings
     """
+
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    E = X.toarray()
+    F = vectorizer.get_feature_names()
+
+    return E, F
