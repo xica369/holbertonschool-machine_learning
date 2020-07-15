ffdb213fe58d70c2ad4088c4cbafbcc25a58ba59
xica369 <761@holbertonschool.com>
2020-07-15 05:19:49 +0000

    update main

--- supervised_learning/0x0E-word_embeddings/4-main.py
+++ supervised_learning/0x0E-word_embeddings/4-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from gensim.test.utils import common_texts
-fasttext_model = __import__('4-fasttext').fasttext_model
+fasttext_model = __import__('4-fasttext').fasttext
 
 print(common_texts[:2])
 ft = fasttext_model(common_texts, min_count=1)
