343adcdea607f2cb4b553de35429fb587db6dc20
xica369 <761@holbertonschool.com>
2020-07-24 03:22:36 +0000

    update

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -4,3 +4,114 @@
 script that creates trains, and validates a keras model
 for the forecasting of BTC
 """
+
+import tensorflow as tf
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import tensorflow.contrib.eager as tfe
+
+# Enable eager execution
+tfe.enable_eager_execution()
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+csv_path = "./preprocess.csv"
+df = pd.read_csv(csv_path)
+
+# the first 3.000.000 rows of the data will be the training dataset
+TRAIN_SPLIT = 3000000
+
+# Setting seed to ensure reproducibility.
+tf.random.set_random_seed(1)
+
+features_considered = ["Volume_BTC", "Volume_USD", "Weighted_Price"]
+features = df[features_considered]
+features.index = df["Date_time"]
+
+dataset = features.values
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+
+dataset = (dataset-data_mean)/data_std
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i, step)
+        data.append(dataset[indices])
+
+        if single_step:
+            labels.append(target[i+target_size])
+        else:
+            labels.append(target[i:i+target_size])
+
+        return np.array(data), np.array(labels)
+
+past_history = 60 * 24
+future_target = 60
+STEP = 60
+
+x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP,
+                                                   single_step=True)
+
+x_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
+                                               TRAIN_SPLIT, None, past_history,
+                                               future_target, STEP,
+                                               single_step=True)
+
+print('Single window of past history : {}'.format(x_train_single[0].shape))
+
+BATCH_SIZE = 256
+BUFFER_SIZE = 10000
+
+train_data_single = tf.data.Dataset.from_tensor_slices((x_train_single, y_train_single))
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((x_val_single, y_val_single))
+val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+single_step_model = tf.keras.models.Sequential()
+single_step_model.add(tf.keras.layers.LSTM(32,
+                                           input_shape=x_train_single.shape[-2:]))
+single_step_model.add(tf.keras.layers.Dense(1))
+
+single_step_model.compile(optimizer=tf.train.AdamOptimizer(), loss='mse')
+
+EVALUATION_INTERVAL = 200
+EPOCHS = 10
+
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+
+def plot_train_history(history, title):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title(title)
+    plt.legend()
+
+    plt.show()
+
+plot_train_history(single_step_history,
+                   'Single Step Training and validation loss')
