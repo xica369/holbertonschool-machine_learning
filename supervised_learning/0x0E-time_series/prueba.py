f8b60933aa78c08cdd3986d010e76dd39dbc9f30
xica369 <761@holbertonschool.com>
2020-07-27 03:38:43 +0000

    update

--- supervised_learning/0x0E-time_series/prueba.py
+++ supervised_learning/0x0E-time_series/prueba.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import pandas as pd
 import tensorflow.contrib.eager as tfe
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Enable eager execution
 tfe.enable_eager_execution()
@@ -14,11 +15,15 @@ tfe.enable_eager_execution()
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 
+"""
 zip_path = tf.keras.utils.get_file(
     origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
     fname='jena_climate_2009_2016.csv.zip',
     extract=True)
 csv_path, _ = os.path.splitext(zip_path)
+"""
+csv_path = "./preprocess.csv"
+
 
 df = pd.read_csv(csv_path)
 print(df.head())
@@ -42,8 +47,8 @@ TRAIN_SPLIT = 300000
 
 tf.random.set_random_seed(13)
 
-uni_data = df['T (degC)']
-uni_data.index = df['Date Time']
+uni_data = df["Volume_BTC"]
+uni_data.index = df["Date_time"]
 print(uni_data.head())
 
 uni_data.plot(subplots=True)
@@ -117,15 +122,16 @@ simple_lstm_model = tf.keras.models.Sequential([
     tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
     tf.keras.layers.Dense(1)
 ])
-
+"""
 simple_lstm_model.compile(optimizer=tf.train.AdamOptimizer(), loss='mae')
 
 for x, y in val_univariate.take(1):
     print(simple_lstm_model.predict(x).shape)
-
+"""
 EVALUATION_INTERVAL = 200
 EPOCHS = 3
 
+"""
 simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
                       steps_per_epoch=EVALUATION_INTERVAL,
                       validation_data=val_univariate, validation_steps=50)
@@ -133,4 +139,103 @@ simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
 for x, y in val_univariate.take(3):
     plot = show_plot([x[0].numpy(), y[0].numpy(),
                       simple_lstm_model.predict(x)[0]], 0, 'Simple LSTM model')
+    plot.show()
+"""
+
+print("####################  MULTIDATA #############################")
+
+
+features_considered = ["Volume_BTC", "Volume_USD", "Weighted_Price"]
+
+features = df[features_considered]
+features.index = df['Date_time']
+print(features.head())
+
+features.plot(subplots=True)
+
+dataset = features.values
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+
+dataset = (dataset-data_mean)/data_std
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i, step)
+        data.append(dataset[indices])
+
+        if single_step:
+            labels.append(target[i+target_size])
+        else:
+            labels.append(target[i:i+target_size])
+
+    return np.array(data), np.array(labels)
+
+past_history = 720
+future_target = 72
+STEP = 6
+
+x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP,
+                                                   single_step=True)
+
+x_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
+                                               TRAIN_SPLIT, None, past_history,
+                                               future_target, STEP,
+                                               single_step=True)
+
+print ('Single window of past history : {}'.format(x_train_single[0].shape))
+
+train_data_single = tf.data.Dataset.from_tensor_slices((x_train_single, y_train_single))
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((x_val_single, y_val_single))
+val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+single_step_model = tf.keras.models.Sequential()
+single_step_model.add(tf.keras.layers.LSTM(32,
+                                           input_shape=x_train_single.shape[-2:]))
+single_step_model.add(tf.keras.layers.Dense(1))
+
+single_step_model.compile(optimizer=tf.train.AdamOptimizer(), loss='mae')
+
+for x, y in val_data_single.take(1):
+    print(single_step_model.predict(x).shape)
+
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+
+def plot_train_history(history, title):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title(title)
+    plt.legend()
+
+    plt.show()
+
+plot_train_history(single_step_history,
+                   'Single Step Training and validation loss')
+
+for x, y in val_data_single.take(3):
+    plot = show_plot([x[0][:, 1].numpy(), y[0].numpy(),
+                      single_step_model.predict(x)[0]], 12,
+                     'Single Step Prediction')
     plot.show()
