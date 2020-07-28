4421e8466f48eb5b77437407bbaf80c4df6b0ac5
xica369 <761@holbertonschool.com>
2020-07-28 22:13:43 +0000

    positional encoding

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -19,3 +19,17 @@ def positional_encoding(max_seq_len, dm):
     a numpy.ndarray of shape (max_seq_len, dm) containing the positional
     encoding vectors
     """
+
+    pos = np.arange(max_seq_len)[:, np.newaxis]
+    i = np.arange(dm)[np.newaxis, :]
+
+    # positional_encoding = pos / (10000 ^ (2i/dmo))
+    positional_encoding = pos / np.power(10000, (2 * (i//2)) / np.float32(dm))
+
+    # sin to even indices in the array; 2i
+    positional_encoding[:, 0::2] = np.sin(positional_encoding[:, 0::2])
+
+    # cos to odd indices in the array; 2i+1
+    positional_encoding[:, 1::2] = np.cos(positional_encoding[:, 1::2])
+
+    return positional_encoding
