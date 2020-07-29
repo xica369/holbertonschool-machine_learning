7117960ff2ea61488092fa33e1df685bd3de5736
xica369 <761@holbertonschool.com>
2020-07-29 00:44:47 +0000

    Scaled Dot Product Attention

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -29,3 +29,21 @@ def sdp_attention(Q, K, V, mask=None):
     - weights a tensor with its last two dimensions as
     (..., seq_len_q, seq_len_v) containing the attention weights
     """
+
+    # matmul_qk shape = (..., seq_len_q, seq_len_k)
+    matmul_qk = tf.matmul(Q, K, transpose_b=True)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    # softmax is normalized on the last axis (seq_len_k) so that the scores
+    weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(weights, V)
+
+    return output, weights
