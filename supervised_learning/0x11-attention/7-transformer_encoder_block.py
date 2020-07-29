2ecba8bb70f4a449b47bfbaeea7adf7b0890b70a
xica369 <761@holbertonschool.com>
2020-07-29 02:23:37 +0000

    Transformer Encoder Block

--- supervised_learning/0x11-attention/7-transformer_encoder_block.py
+++ supervised_learning/0x11-attention/7-transformer_encoder_block.py
@@ -7,7 +7,7 @@ to create an encoder block for a transformer
 """
 
 import tensorflow as tf
-MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+MultiHeadAttention = __import__('6-multi_head_attention').MultiHeadAttention
 
 
 class EncoderBlock(tf.keras.layers.Layer):
@@ -33,6 +33,15 @@ class EncoderBlock(tf.keras.layers.Layer):
         - dropout2: the second dropout layer
         """
 
+        super(EncoderBlock, self).__init__()
+        self.mha = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation="relu")
+        self.dense_output = tf.keras.layers.Dense(dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
     def call(self, x, training, mask=None):
         """
         - x: a tensor of shape (batch, input_seq_len, dm)containing the input
@@ -43,3 +52,14 @@ class EncoderBlock(tf.keras.layers.Layer):
         Returns: a tensor of shape (batch, input_seq_len, dm) containing the
         block’s output
         """
+
+        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
+
+        output = self.dense_hidden(out1)
+        output = self.dense_output(output)  # (batch_size, input_seq_len, d_model)
+        output = self.dropout2(output, training=training)
+        out2 = self.layernorm2(out1 + output)  # (batch_size, input_seq_len, d_model)
+
+        return out2
