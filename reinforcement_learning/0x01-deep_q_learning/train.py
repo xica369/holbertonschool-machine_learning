6d65aa421a8542931e043af93d676f97eca18f8b
xica369 <761@holbertonschool.com>
2020-08-18 16:42:32 +0000

    update train

--- reinforcement_learning/0x01-deep_q_learning/train.py
+++ reinforcement_learning/0x01-deep_q_learning/train.py
@@ -1,81 +1,116 @@
 #!/usr/bin/env python3
 
 """
-Script to train an agent that can play Atari’s Breakout
+Functions to train an agent that can play Atari’s Breakout
 """
 
 import gym
-import tensorflow as tf
-import tensorflow.keras as K
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Convolution2D, Permute, Activation
+from rl.callbacks import ModelIntervalCheckpoint, FileLogger
+from rl.processors import Processor
+from PIL import Image
+from keras.optimizers import Adam
 from rl.agents.dqn import DQNAgent
 from rl.policy import EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 
 
-class AtariBreakoutEnvManager():
-    """
-    This class will manage our Atari Breakout enviroment
-    """
+class InputProcessor(Processor):
+    def process_observation(self, observation):
 
-    def __init__(self):
-        """
-        class constructor
-        """
-        self.env = gym.make("Breakout-v0")
-        self.env.reset()
-        self.current_screen = None
-        self.donde = False
+         # (height, width, channel)
+        assert observation.ndim == 3
+        img = Image.fromarray(observation)
 
-    # Wrapped Functions
+        # resize and convert to grayscale
+        img = img.resize((84, 84), Image.ANTIALIAS).convert('L')
+        processed_observation = np.array(img)
+        assert processed_observation.shape == (84, 84)
 
-    def reset(self):
-        """
-        environment is reset to a starting state
-        returns an initial observation from the environment.
-        """
-        self.env.reset()
-        self.current_screen = None
+        # saves storage in experience memory
+        return processed_observation.astype('uint8')
 
-    def close(self):
+    def process_state_batch(self, batch):
         """
-        close the environment
+        We could perform this processing step in `process_observation`.
+        In this case, however,
+        we would need to store a `float32` array instead, which is 4x more
+        memory intensive than
+        an `uint8` array. This matters if we store 1M observations.
         """
-        self.env.close()
+        processed_batch = batch.astype('float32') / 255.
+        return processed_batch
 
-    def render(self, mode='human'):
-        """
-        render the current state to the screen.
-        """
-        return self.env.render(mode)
 
-    def num_actions_available(self):
-        """
-        returns the number of actions available to an agent in the
-        environment
-        """
-        return self.env.action_space.n
+def build_model(state_size, num_actions):
+    input_shape = (4,) + state_size
+    model = Sequential()
+    model.add(Permute((2, 3, 1), input_shape=input_shape))
+    model.add(Convolution2D(32, 8, 8, subsample=(4, 4)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3, subsample=(1, 1)))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(num_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+    return model
 
-    def take_action(self, action):
-        """
-        Taking An Action In The Environment
 
-        call step() on the environment, which will execute the given
-        action taken by the agent in the environment.
+def build_callbacks(env_name):
+    checkpoint_weights_filename = 'dqn_' + env_name + '_weights_{step}.h5f'
+    log_filename = 'dqn_{}_log.json'.format(env_name)
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename,
+                                         interval=250000)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+    return callbacks
 
-        Return:
-        the reward obtained
-        """
-        _, reward, self.done, _ = self.env.step(action.item())
 
-        return reward
+if __name__ == '__main__':
 
-    def just_starting(self):
-        """
-        Starting An Episode
+    ENV_NAME = 'BreakoutDeterministic-v4'
+    INPUT_SHAPE = (84, 84)
+    WINDOW_LENGTH = 4
+    # Get the environment and extract the number of actions.
+    env = gym.make(ENV_NAME)
+    np.random.seed(42)
+    env.seed(42)
+    num_actions = env.action_space.n
+    input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 
-        returns True when the current_screen is None and
-        returns False otherwise
-        """
-        return self.current_screen is None
+    model = build_model(INPUT_SHAPE, num_actions)
+    memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
+    processor = InputProcessor()
+    policy = EpsGreedyQPolicy()
 
+    dqn = DQNAgent(model=model,
+                   nb_actions=num_actions,
+                   policy=policy,
+                   memory=memory,
+                   processor=processor,
+                   nb_steps_warmup=50000,
+                   gamma=.99,
+                   target_model_update=10000,
+                   train_interval=4,
+                   delta_clip=1.)
+
+    dqn.compile(Adam(lr=.00025), metrics=['mae'])
+    callbacks = build_callbacks(ENV_NAME)
+    dqn.fit(env,
+            nb_steps=3,
+            log_interval=10000,
+            visualize=False,
+            verbose=2,
+            callbacks=callbacks)
+
+    # After training is done, we save the final weights.
+    dqn.save_weights("policy.h5", overwrite=True)
+
+    # Finally, evaluate our algorithm for 5 episodes.
+    dqn.test(env, nb_episodes=1, visualize=True)
