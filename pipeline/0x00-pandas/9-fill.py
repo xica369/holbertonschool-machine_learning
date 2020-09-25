be42729e07118d476c9b5fd47ebcd7ca5d58ae1e
xica369 <761@holbertonschool.com>
2020-09-25 15:57:17 +0000

    update fill

--- pipeline/0x00-pandas/9-fill.py
+++ pipeline/0x00-pandas/9-fill.py
@@ -6,10 +6,10 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 df.drop(["Weighted_Price"], axis="columns", inplace=True)
-df["High"].fillna(method='pad', inplace=True)
-df["Low"].fillna(method='pad', inplace=True)
-df["Open"].fillna(method='pad', inplace=True)
 df["Close"].fillna(method='pad', inplace=True)
+df["High"].fillna(df.Close.shift(1), inplace=True)
+df["Low"].fillna(df.Close.shift(1), inplace=True)
+df["Open"].fillna(df.Close.shift(1), inplace=True)
 df["Volume_(Currency)"].fillna(0, inplace=True)
 df["Volume_(BTC)"].fillna(0, inplace=True)
 
