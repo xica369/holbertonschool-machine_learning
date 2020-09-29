0c9f5d24a38d2623eb97d16ab4a11a80dadd1140
xica369 <761@holbertonschool.com>
2020-09-29 21:40:38 +0000

    rocket frequency

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -17,3 +17,32 @@ order them by alphabetic order (A to Z)
 import requests
 
 if __name__ == "__main__":
+
+    url = "https://api.spacexdata.com/v4/launches"
+    response = requests.get(url)
+    rockets = {}
+
+    if response.status_code == 200:
+        launches = response.json()
+
+        for launch in launches:
+            rocket = launch["rocket"]
+            if rocket in rockets:
+                rockets[rocket] += 1
+            else:
+                rockets[rocket] = 1
+
+    rockets_name = {}
+    for key, val in rockets.items():
+        resp = requests.get("https://api.spacexdata.com/v4/rockets/" + key)
+        name = resp.json()["name"]
+        rockets_name[name] = rockets[key]
+
+    del rockets
+
+    rockets = sorted(rockets_name.items(),
+                     key=lambda x: (x[1], x[0]),
+                     reverse=True)
+
+    for rocket in rockets:
+        print("{}: {}".format(rocket[0], rocket[1]))
