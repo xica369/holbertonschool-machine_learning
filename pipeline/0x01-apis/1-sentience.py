e4fdef99f6e60d1e7e2cc925a28d23628c4f5a4c
xica369 <761@holbertonschool.com>
2020-09-29 02:51:45 +0000

    update sentience

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -5,10 +5,33 @@ By using the Swapi API, create a method that returns the list of names
 of the home planets of all sentient species.
 """
 
-import request
+import requests
 
 
 def sentientPlanets():
     """
     returns the list of names of the home planets of all sentient species\
     """
+
+    url = "https://swapi-api.hbtn.io/api/species"
+    home_planets = []
+
+    while url:
+        response_species = requests.get(url)
+
+        if response_species.status_code == 200:
+            resp_species = response_species.json()
+            result_species = resp_species["results"]
+
+            for specie in result_species:
+                if specie["designation"].lower() == "sentient":
+                    if specie["homeworld"]:
+                        response_homeworld = requests.get(specie["homeworld"])
+                        resp_homeworld = response_homeworld.json()
+                        home_planets.append(resp_homeworld["name"])
+
+            url = resp_species["next"]
+        else:
+            url = None
+
+    return home_planets
