17261f54b08bb635de6d283de4c787427a2c1945
xica369 <761@holbertonschool.com>
2020-10-01 20:38:22 +0000

    update join

--- pipeline/0x02-databases/9-no_genre.sql
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -4,7 +4,7 @@
 -- tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
