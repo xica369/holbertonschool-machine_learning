392a3fa42c50f97950f450f244abb09eb5e0d3e1
xica369 <761@holbertonschool.com>
2020-10-03 03:06:42 +0000

    [mongoDb] look specific topic in python

---
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/urs/bin/env python3
+
+"""
+function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    - mongo_collection will be the pymongo collection object
+    - topic (string) will be topic searched
+
+    Return list of school having a specific topic
+    """
+
+    topic = mongo_collection.find({"topics": {"$in": [topic]}})
+
+    return topic
