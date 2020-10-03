6979bf6c9508520df3bdacbf55cab2dfa7b98395
xica369 <761@holbertonschool.com>
2020-10-03 04:57:09 +0000

    update

--- pipeline/0x02-databases/102-need_meeting.sql
+++ pipeline/0x02-databases/102-need_meeting.sql
@@ -1,1 +1,5 @@
 -- creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80;
