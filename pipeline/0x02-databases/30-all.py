144d650b7bec80dedf116caf82aefdbdb7f0964c
xica369 <761@holbertonschool.com>
2020-10-03 02:39:31 +0000

    [mongoDb] list all documents in python

---
+++ pipeline/0x02-databases/30-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    - mongo_collection will be the pymongo collection object
+
+    Return an empty list if no document in the collection
+    """
+
+    return mongo_collection.find()
