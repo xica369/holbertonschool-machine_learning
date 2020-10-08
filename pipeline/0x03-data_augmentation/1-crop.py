b39af8db82ca83816f7c25c83699945902630f7b
xica369 <761@holbertonschool.com>
2020-10-08 20:53:08 +0000

    crop

---
+++ pipeline/0x03-data_augmentation/1-crop.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Crop
+"""
+
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """
+    Function that performs a random crop of an image:
+
+    - image is a 3D tf.Tensor containing the image to crop
+    - size is a tuple containing the size of the crop
+
+    Returns the cropped image
+    """
+
+    return tf.image.random_crop(image, size)
